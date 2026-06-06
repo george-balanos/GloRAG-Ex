d93ae2ca96e9142d2441266dcb62af581bea746b
Evangelos Chasanis <110494500+vcodestar@users.noreply.github.com>
2026-06-06 18:55:15 +0300

    shapley implementation

---
+++ competitors/RAGEX-RAGE-SHAPLEY/LLM/run_tests.py
@@ -0,0 +1,14 @@
+from LLM.LLMWrapper import LLMWrapper
+
+if __name__ == "__main__":
+    lw = LLMWrapper()
+    # lw.evaluate_file("master_synthetic_dataset.csv", "llm_only")
+    # lw.evaluate_file("master_synthetic_dataset.csv", "rag")
+    lw.compare_answers("master_synthetic_dataset", "remove_sentence")
+    lw.extract_impactful_changes("master_synthetic_dataset", "remove_sentence")
+
+    # lw.compare_answers("master_synthetic_dataset", "rage")
+    # lw.extract_impactful_changes("master_synthetic_dataset", "rage")
+
+    # lw.compare_answers("master_synthetic_dataset", "remove_word")
+    # lw.extract_impactful_changes("master_synthetic_dataset", "remove_word")
