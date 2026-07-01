4ed277ecaca89fa5c13923262ef75f0bd60b8163
george-balanos <g.balanos@uoi.gr>
2026-07-01 15:22:58 +0000

    Create scripts for RAG-Ex, noise resistance, and positional-bias

---
+++ competitors/RAGEX-RAGE-SHAPLEY/run_rag_ex_permutations.sh
@@ -0,0 +1,19 @@
+python3 LLM/ragex_permutation.py --input /home/gbalanos/GloRAG-Ex/competitors/RAGEX-RAGE-SHAPLEY/experiments/synthetic/synthetic_remove_sentence_analysis.json --working-dir /home/gbalanos/GloRAG-Ex/code/KGs/lightrag/synthetic --questions-file /home/gbalanos/GloRAG-Ex/code/src/sampled_questions/sample_100_synthetic_ff.json --dataset "synthetic" --output /home/gbalanos/GloRAG-Ex/competitors/RAGEX-RAGE-SHAPLEY/LLM/permutations_ragex/synthetic_ff_ragex_permutation.json
+
+
+
+python3 LLM/ragex_permutation.py --input /home/gbalanos/GloRAG-Ex/competitors/RAGEX-RAGE-SHAPLEY/experiments/hotpotqa/hotpotqa_remove_sentence_analysis.json --working-dir /home/gbalanos/GloRAG-Ex/code/KGs/lightrag/hotpotqa --questions-file /home/gbalanos/GloRAG-Ex/code/src/sampled_questions/sample_50_hotpotqa_ft.json --dataset "hotpotqa" --output /home/gbalanos/GloRAG-Ex/competitors/RAGEX-RAGE-SHAPLEY/LLM/permutations_ragex/hotpotqa_ft_ragex_permutation.json
+
+python3 LLM/ragex_permutation.py --input /home/gbalanos/GloRAG-Ex/competitors/RAGEX-RAGE-SHAPLEY/experiments/hotpotqa/hotpotqa_remove_sentence_analysis.json --working-dir /home/gbalanos/GloRAG-Ex/code/KGs/lightrag/hotpotqa --questions-file /home/gbalanos/GloRAG-Ex/code/src/sampled_questions/sample_60_hotpotqa_ff.json --dataset "hotpotqa" --output /home/gbalanos/GloRAG-Ex/competitors/RAGEX-RAGE-SHAPLEY/LLM/permutations_ragex/hotpotqa_ff_ragex_permutation.json
+
+
+
+python3 LLM/ragex_permutation.py --input /home/gbalanos/GloRAG-Ex/competitors/RAGEX-RAGE-SHAPLEY/experiments/musique/musique_remove_sentence_analysis.json --working-dir /home/gbalanos/GloRAG-Ex/code/KGs/lightrag/musique --questions-file /home/gbalanos/GloRAG-Ex/code/src/sampled_questions/new_sample_70_musique_ft.json --dataset "musique" --output /home/gbalanos/GloRAG-Ex/competitors/RAGEX-RAGE-SHAPLEY/LLM/permutations_ragex/musique_ft_ragex_permutation.json
+
+python3 LLM/ragex_permutation.py --input /home/gbalanos/GloRAG-Ex/competitors/RAGEX-RAGE-SHAPLEY/experiments/musique/musique_remove_sentence_analysis.json --working-dir /home/gbalanos/GloRAG-Ex/code/KGs/lightrag/musique --questions-file /home/gbalanos/GloRAG-Ex/code/src/sampled_questions/sample_90_musique_ff.json --dataset "musique" --output /home/gbalanos/GloRAG-Ex/competitors/RAGEX-RAGE-SHAPLEY/LLM/permutations_ragex/musique_ff_ragex_permutation.json
+
+
+python3 LLM/ragex_permutation.py --input /home/gbalanos/GloRAG-Ex/competitors/RAGEX-RAGE-SHAPLEY/experiments/2wiki/2wiki_remove_sentence_analysis.json --working-dir /home/gbalanos/GloRAG-Ex/code/KGs/lightrag/2wiki --questions-file /home/gbalanos/GloRAG-Ex/code/src/sampled_questions/sample_45_2wiki_ft.json --dataset "2wiki" --output /home/gbalanos/GloRAG-Ex/competitors/RAGEX-RAGE-SHAPLEY/LLM/permutations_ragex/2wiki_ft_ragex_permutation.json
+
+
+python3 LLM/ragex_permutation.py --input /home/gbalanos/GloRAG-Ex/competitors/RAGEX-RAGE-SHAPLEY/experiments/2wiki/2wiki_remove_sentence_analysis.json --working-dir /home/gbalanos/GloRAG-Ex/code/KGs/lightrag/2wiki --questions-file /home/gbalanos/GloRAG-Ex/code/src/sampled_questions/sample_100_2wiki_ff.json --dataset "2wiki" --output /home/gbalanos/GloRAG-Ex/competitors/RAGEX-RAGE-SHAPLEY/LLM/permutations_ragex/2wiki_ff_ragex_permutation.json
